15660904aaea74e7ef7697d4ec56369b4504a4c5
ashishkandu <ashishkandu43@gmail.com>
2024-02-23 18:28:19 +0545

    merge restore_database.py and run.py

--- app/run.py
+++ app/run.py
@@ -1,9 +1,35 @@
+import json
 from nepali_datetime import datetime as np_datetime
+from drive_database.credential_handler import get_credentials
+from drive_database.database_operations import restore
+from settings import DRIVE_CACHE_PATH
+from drive_database.drive import GoogleDriveFile, download_drive_file, retrive_latest_file_by_pattern
 from src.date_helpers import get_month_name_np
 from src.filingmonth import FilingMonth
 from src.menu import Menu, MenuOption, ReportMenu
 from src.nepalidateselector import NepaliDateSelector
 from src.report_generator import ReportGenerator
+
+
+def perform_restore(file: GoogleDriveFile, creds) -> None:
+    if DRIVE_CACHE_PATH.exists():
+        with DRIVE_CACHE_PATH.open('r') as f:
+            info: dict = json.load(f)
+        if info['name'] == file['name']:
+            print("Database already restored from the latest backup!")
+            return None
+        else:
+            print(
+                f"Database was restored from another backup on {info['datetime']}")
+            print(f"Current backup: {info['name']}")
+            print(f"Latest backup: {file['name']}")
+            choice = input("Do you want to restore latest backup? [y/N] ")
+            if choice.lower() != 'y':
+                return None
+            print("Database will be restored from the latest backup!")
+
+    filepath = download_drive_file(creds, file)
+    restore(filepath)
 
 
 def get_month_report(**kwargs) -> None:
@@ -50,6 +76,13 @@ def select_custom_date_range() -> None:
 
 
 if __name__ == "__main__":
+
+    file_pattern = r"VatBillingSoftware_\d+_\d+\.bak"
+    creds = get_credentials()
+    latest_file = retrive_latest_file_by_pattern(creds, file_pattern)
+
+    if latest_file is not None:
+        perform_restore(latest_file, creds)
 
     now = np_datetime.now()
     previous_month = get_month_name_np(now.month - 1)
